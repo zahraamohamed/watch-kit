06acb9935278d673032a5d978a086ef954758837
Zahraa resen <32715755+zahraamohamed@users.noreply.github.com>
2021-11-02 13:20:21 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+input.onGesture(Gesture.Shake, function () {
+    i = randint(1, 6)
+})
+let i = 0
+basic.showIcon(IconNames.Duck)
+basic.forever(function () {
+    if (i == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (i == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (i == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (i == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (i == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (i == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
